a5d34bc89e11c132cbe13801ff972d6ea02f3a61
Sacha Herszkorn <herszkorn.sacha@gmail.com>
2020-11-29 18:58:31 +0100

    [MOD] Navbar and variables

--- src/standalone/Navbar/Navbar.js
+++ src/standalone/Navbar/Navbar.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 import './Navbar.scss';
 
 function Navbar() {
+  const location = useLocation();
+  const isOnVotePage = location.pathname === '/vote';
+
   return (
     <nav className="navbar">
-      <h1>catmash</h1>
+      <div className="nav-items nav-items-left">
+        <Link className="home-link" to="/">
+          <h1>catmash 🐈</h1>
+        </Link>
+      </div>
+      <div className="nav-items nav-items-right">
+        {!isOnVotePage && (
+          <Link className="vote-link" to="/vote">
+            <h1>
+              start voting <span>✋</span>
+            </h1>
+          </Link>
+        )}
+      </div>
     </nav>
   );
 }
